f4b43cfb00ccbe8475f87cb998637d3a0c4d7971
Michael Müller <haimich@gmx.net>
2014-08-14 22:10:58 +0000

    added command dependency for arg parsing and added all relevant command line arguments

--- bin/knick-knack.js
+++ bin/knick-knack.js
@@ -1,14 +1,30 @@
+#!/usr/bin/env node
+
 'use strict';
 
-//Command line argument parser
-//
-//Options:
-// --help, -h:         show usage information
-// no options:         list all project templates
-// init:               initialize a valid knick-knack project templates folder
-// xp/rest-client:     generate a new xp project of type rest-client
-// check:              check which "features" the project in the current folder has (eg. a GIT project, Jenkins job,...)
-// create git-project: create a Git project for the project in the current folder
-// create jenkins-job: create a Jenkins job for the project in the current folder
-// add general/readme: add a readme partial to an existing project
-// -d ~/.knick-knack:  specify the folder where knick-knack should look for project templates [optional]
+var program = require('commander');
+var projectVersion = require('../package.json').version;
+
+program
+  .version(projectVersion)
+  .option('-d, --directory [folder]', 'Specify the folder where knick-knack should look for project templates [~/.knick-knack]', '~/.knick-knack');
+
+//specify additional help text
+program.on('--help', function(){
+  console.log('  Commands:');
+  console.log('');
+  console.log('    $ knick-knack                    List all available project templates');
+  console.log('    $ knick-knack init               Initialize a valid knick-knack project templates folder');
+  console.log('    $ knick-knack check              Check which "features" the project in the current folder has (eg. a GIT project, Jenkins job,...)');
+  console.log('    $ knick-knack xp/rest-client     Generate a new xp project of type rest-client');
+  console.log('    $ knick-knack add general/readme Add a readme partial to an existing project')
+  console.log('    $ knick-knack create git-project Create a Git project for the project in the current folder');
+  console.log('    $ knick-knack create jenkins-job Create a Jenkins job for the project in the current folder');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+console.log('You chose: ');
+console.log('Directory: %s', program.directory);
+console.log('Remaining args:', program.args);
