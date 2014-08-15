445b9536803cf512eabf6a1df60d545a86687d42
Michael Müller <haimich@gmx.net>
2014-08-15 12:51:35 +0000

    fixed linting errors

--- bin/knick-knack.js
+++ bin/knick-knack.js
@@ -17,7 +17,7 @@ program.on('--help', function(){
   console.log('    $ knick-knack init               Initialize a valid knick-knack project templates folder');
   console.log('    $ knick-knack check              Check which "features" the project in the current folder has (eg. a GIT project, Jenkins job,...)');
   console.log('    $ knick-knack xp/rest-client     Generate a new xp project of type rest-client');
-  console.log('    $ knick-knack add general/readme Add a readme partial to an existing project')
+  console.log('    $ knick-knack add general/readme Add a readme partial to an existing project');
   console.log('    $ knick-knack create git-project Create a Git project for the project in the current folder');
   console.log('    $ knick-knack create jenkins-job Create a Jenkins job for the project in the current folder');
   console.log('');
