93d0d1b3d71440ed353114c7380eac0c0045b843
Michael Müller <haimich@gmx.net>
2014-08-15 13:23:37 +0000

    added sinon and chai, renamed test folder to spec

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
-    mocha  = require('gulp-mocha');
+    mocha  = require('gulp-mocha'),
+    chai   = require('chai'),
+    sinon  = require('sinon');
 
 var paths = {
   scripts: ['./lib/**/*.js', './bin/**/*.js'],
-  tests: ['./test/**/*.js']
+  tests: ['./spec/**/*.js']
 };
 
 gulp.task('test', function () {
   return gulp.src(paths.tests, { read: false })
-    .pipe(mocha({reporter: 'nyan'}));
+    .pipe(mocha({
+      reporter: 'nyan',
+      globals: {
+        should: chai.should(),
+        sinon: sinon
+      }
+    }));
 });
 
 gulp.task('lint', function() {
---
+++ spec/test.js
@@ -0,0 +1,5 @@
+describe('A test', function() {
+  it('should be true', function() {
+    true.should.be.true;
+  })
+})
