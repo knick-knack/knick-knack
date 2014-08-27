3ca6eb0b1f3b352301576c13515c912d5d31a23f
Johannes Kissel <johannes.kissel@junghans-schneider.de>
2014-08-27 23:27:05 +0200

    fixed template tests

--- spec/lib/templatesTest.js
+++ spec/lib/templatesTest.js
@@ -1,38 +1,35 @@
-var sut           = require('../../lib/template'),
+var expect        = require('chai').expect,
+    sut           = require('../../lib/template'),
     exampleFolder = process.cwd() + '/spec/testdata/';
 
-// describe('isValidProjectTemplate', function() {
-//   describe('when given a valid project folder', function() {
-//     it('should return true', function() {
-//       sut.isValidProjectTemplate(exampleFolder + '/projects/python/fabric').should.be.true;
-//     });
-//   });
+describe('isValidProjectTemplate', function() {
+  describe('when given a valid project folder', function() {
+    it('should return true', function() {
+      expect(sut.isValidProjectTemplate(exampleFolder + '/python-fabric')).to.be.true;
+    });
+  });
 
-//   describe('when given an invalid project folder', function() {
-//     it('should return false for container folders', function() {
-//       sut.isValidProjectTemplate(exampleFolder + '/projects/python').should.be.false;
-//     });
+  describe('when given an invalid project folder', function() {
+    it('should return false for invalid folders', function() {
+      expect(sut.isValidProjectTemplate(exampleFolder + '/invalid')).to.be.false;
+      expect(sut.isValidProjectTemplate(exampleFolder)).to.be.false;
+    });
+  });
+});
 
-//     it('should return false for invalid folders', function() {
-//       sut.isValidProjectTemplate(exampleFolder + '/projects/invalid').should.be.false;
-//       sut.isValidProjectTemplate(exampleFolder + '/projects').should.be.false;
-//     });
-//   });
-// });
+describe('readDescription()', function() {
+  it('should output the description from the config file', function() {
+    expect(sut.readDescription(exampleFolder + '/python-fabric')).to.equal('Create a Fabric project.');
+  });
+});
 
-// describe('readDescription()', function() {
-//   it('should output the description from the config file', function() {
-//     sut.readDescription(exampleFolder + '/projects/python/fabric').should.equal('Create a Fabric project.');
-//   });
-// });
-
-// describe('readConfigFile()', function() {
-//   it('should read the config file and return the content as an object', function() {
-//     var config = sut.readConfigFile(exampleFolder + '/projects/python/fabric');
-//     config.should.be.ok;
-//     config.description.should.equal('Create a Fabric project.');
-//     config.partials[0].should.equal('general/readme');
-//     config.partials[1].should.equal('python/config');
-//     config.defaults.name.should.equal('test-project');
-//   });
-// });
+describe('readConfigFile()', function() {
+  it('should read the config file and return the content as an object', function() {
+    var config = sut.readConfigFile(exampleFolder + '/python-fabric');
+    expect(config).to.be.ok;
+    expect(config.description).to.equal('Create a Fabric project.');
+    expect(config.partials[0]).to.equal('general/readme');
+    expect(config.partials[1]).to.equal('python/config');
+    expect(config.defaults.name).to.equal('test-project');
+  });
+});
