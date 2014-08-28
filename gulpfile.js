e5fb287fa084259bf7477a1cfbfddc7799a704aa
Johannes Kissel <johannes.kissel@junghans-schneider.de>
2014-08-28 11:16:11 +0200

    removed unused dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,6 @@
 var gulp   = require('gulp'),
     jshint = require('gulp-jshint'),
-    mocha  = require('gulp-mocha'),
-    chai   = require('chai'),
-    sinon  = require('sinon');
+    mocha  = require('gulp-mocha');
 
 var paths = {
   scripts: ['./lib/**/*.js', './bin/**/*.js'],
@@ -16,13 +14,13 @@ gulp.task('test', function () {
     }));
 });
 
-gulp.task('lint', function() {
+gulp.task('lint', function () {
   return gulp.src(paths.scripts)
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.start('test');
   gulp.watch(paths.scripts, ['test']);
   gulp.watch(paths.tests, ['test']);
