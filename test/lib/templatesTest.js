b4934c140228fc10ef116a96dbadb43dd548dd50
Michael Mueller <haimich@gmx.net>
2014-09-08 21:29:48 +0000

    added tests for getRequiredVariables

--- test/lib/templatesTest.js
+++ test/lib/templatesTest.js
@@ -28,8 +28,42 @@ describe('readConfigFile()', function() {
     var config = sut.readConfigFile(exampleFolder + '/python-fabric');
     expect(config).to.be.ok;
     expect(config.description).to.equal('Create a Fabric project.');
-    expect(config.partials[0]).to.equal('general/readme');
-    expect(config.partials[1]).to.equal('python/config');
+    expect(config.partials).to.include('general/readme');
+    expect(config.partials).to.include('python/config');
     expect(config.variables.name).to.equal('test-project');
+  });
+});
+
+describe('getRequiredVariables()', function() {
+  describe('when reading a template without partials', function() {
+    it('should gather all variables used in the template', function() {
+      var result = sut.getRequiredVariables(exampleFolder, exampleFolder + '/python-config');
+      expect(result).not.to.be.empty;
+      expect(result).to.include({
+        name: 'user'
+      });
+      expect(result).not.to.include({
+        name: 'yourscript'
+      });
+    });
+    
+    it('should add all variables used in the config files', function() {
+      var result = sut.getRequiredVariables(exampleFolder, exampleFolder + '/python-config');
+      expect(result).not.to.be.empty;
+      expect(result).to.include({
+        name: 'name',
+        default: 'Sample'
+      });
+      expect(result).to.include({
+        name: 'gitlabGroup',
+        default: 'onetwothree',
+        ask: 'Name please?'
+      });
+      expect(result).to.include({
+        name: 'projectType',
+        default: 'maven',
+        options: ['maven', 'grunt', 'gradle']
+      });
+    });
   });
 });
