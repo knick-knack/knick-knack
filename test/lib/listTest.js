a20f43ff692cc389022648f07d557f524fd2dcc2
Johannes Kissel <johannes.kissel@junghans-schneider.de>
2014-09-16 11:10:41 +0200

    added initial list() implementation

--- test/lib/listTest.js
+++ test/lib/listTest.js
@@ -1,18 +1,26 @@
 var sut = require('../../lib/list'),
+    exampleFolder = process.cwd() + '/test/testdata',
     expect = require('chai').expect;
 
-describe('listTemplates', function() {
-  describe('when no folder given', function() {
-    it('should throw', function() {
-      expect(function() { sut.listTemplates(process.cwd() + '/no-folder'); }).to.throw(Error);
+describe('listTemplates', function () {
+
+  describe('when no directory given', function() {
+    it('should throw', function () {
+      expect(function () { sut.listTemplates(process.cwd() + '/no-directory'); }).to.throw(Error);
     });
   });
 
-  describe('when given a folder with valid templates', function() {
-    it('should return a list of the names of the project templates', function() {
-      var result = sut.listTemplates(process.cwd() + '/test/testdata/');
-      expect(result.length).to.equal(3);
-      expect(result.indexOf('python-fabric: Create a Fabric project.')).to.not.equal(-1);
+  describe('when given a directory with valid templates', function () {
+    var list = sut.listTemplates(exampleFolder);
+
+    it('should return a list of project templates', function () {
+      expect(list.length).to.equal(4);
+    });
+    it('should look only one level deep', function () {
+      expect(list.indexOf('too-deep')).to.equal(-1);
+    });
+    it('should return only templates with config.yml', function () {
+      expect(list.indexOf('no-config')).to.equal(-1);
     });
   });
-})
+});
